70e6b77f938b8b647be069e96a3021d1cd933e16
IrisSkledar <iris.skledar@student.um.si>
2025-03-28 20:56:26 +0100

    Shranjene spremembe pred prehodom na drug branch

--- naloga1.cpp
+++ naloga1.cpp
@@ -5,6 +5,28 @@
 #include <sstream>
 using namespace std;
 
-int main(int argc, char* argv[]) {
+bool Branje_Stevil(vector<int> &vec, const char s[]) {
+
+}
+
+void Izpis_Stevil(int* polje, unsigned int velikost) {
+
+}
+
+int getBit(unsigned char num, int k) {
+
+}
+
+void countingSortByBit(vector<unsigned char>& A, int k) {
+
+}
+
+
+int main(int argc, char* argv[]) { // Kako zaženemo program v konzoli
+    if (argc != 2) {
+        cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka>" << endl;
+        return 1;
+    }
+
     return 0;
 }
