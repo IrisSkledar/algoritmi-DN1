04bb069b2c09d8a8117a11418e3297b9df037d27
IrisSkledar <iris.skledar@student.um.si>
2025-03-28 22:16:33 +0100

    countingSortByBit funkcija implementirana v main + razlaga v komentarjih

--- naloga1.cpp
+++ naloga1.cpp
@@ -17,10 +17,11 @@ int getBit(unsigned char num, int k) {
     return (num >> k) & 1;
 }
 
+// Stabilni counting sort po k-tem bitu
 void countingSortByBit(vector<unsigned char>& A, int k) {
     int n = A.size();
     vector<unsigned char> output(n);
-    vector<int> count(2, 0); 
+    vector<int> count(2, 0);// za bite 0 in 1
 
     vector<int> bits(n);
     for (int i = 0; i < n; i++) {
@@ -28,8 +29,8 @@ void countingSortByBit(vector<unsigned char>& A, int k) {
         count[bits[i]]++;
     }
 
-    count[1] += count[0];
-
+    count[1] += count[0];// kumulativno štetje
+    // Stabilno urejanje
     for (int i = n - 1; i >= 0; i--) {
         int b = bits[i];
         output[--count[b]] = A[i];
@@ -45,6 +46,25 @@ int main(int argc, char* argv[]) { // Kako zaženemo program v konzoli
         cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka>" << endl;
         return 1;
     }
+    // --Pretvori vektor v unsigned char, preveri mejne vrednosti
+    vector<unsigned char> stevila;
+    for (int val : vnos) {
+        if (val < 0 || val > 255) {
+            cerr << "Napaka: število izven razpona [0, 255]: " << val << endl;
+            return 1;
+        }
+        stevila.push_back(static_cast<unsigned char>(val));
+    }
 
+    // Binarni radix sort
+    for (int k = 0; k < 8; k++) {
+        countingSortByBit(stevila, k);
+    }
+
+    //-- Pretvori nazaj v int* za izpis
+    vector<int> izhod;
+    for (unsigned char val : stevila) {
+        izhod.push_back(static_cast<int>(val));
+    }
     return 0;
 }
