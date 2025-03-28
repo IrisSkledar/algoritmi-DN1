9b92927fcae77d0d7acbdde12bee3a4ee5c54648
IrisSkledar <iris.skledar@student.um.si>
2025-03-28 22:10:56 +0100

    countingSortByBit funkcija

--- naloga1.cpp
+++ naloga1.cpp
@@ -18,6 +18,24 @@ int getBit(unsigned char num, int k) {
 }
 
 void countingSortByBit(vector<unsigned char>& A, int k) {
+    int n = A.size();
+    vector<unsigned char> output(n);
+    vector<int> count(2, 0); 
+
+    vector<int> bits(n);
+    for (int i = 0; i < n; i++) {
+        bits[i] = getBit(A[i], k);
+        count[bits[i]]++;
+    }
+
+    count[1] += count[0];
+
+    for (int i = n - 1; i >= 0; i--) {
+        int b = bits[i];
+        output[--count[b]] = A[i];
+    }
+
+    A = output;
 
 }
 
